424a0b5c56ed516020f20ea6f557c93b503af817
yangzhichao <yangzhichao@unfae.com>
2015-11-23 18:41:05 +0800

    配置文件

---
+++ revolution-web/src/main/java/org/autumn/revolution/web/controller/UserController.java
@@ -0,0 +1,10 @@
+package org.autumn.revolution.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yangzhichao on 15/11/23.
+ */
+@RestController
+public class UserController {
+}
