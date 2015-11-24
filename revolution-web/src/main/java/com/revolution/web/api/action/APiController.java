714b8c1c7098813f89233409dbbf3a3ca394f079
=xiangruo@outlook.com <=xiangruo@outlook.com>
2015-11-24 16:18:26 +0800

    提交,增加注释

--- revolution-web/src/main/java/com/revolution/web/api/action/APiController.java
+++ revolution-web/src/main/java/com/revolution/web/api/action/APiController.java
@@ -10,16 +10,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.revolution.web.api.bean.JsonMessageIn;
 
+/**
+ * @author xiangruo 想做一个解析json和xml的接口
+ */
 @Controller
 public class APiController {
 
 	private static final Logger logger = LoggerFactory.getLogger(APiController.class);
 
-	@RequestMapping(value = "/json/api", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	/**
+	 * json--->bean
+	 * @param body
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/json/api", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody ResponseEntity handleJSONRequest(@RequestBody JsonMessageIn body, HttpServletRequest request) {
 		logger.info("APiController handleJSONRequest:{}", body.toString());
 		return new ResponseEntity<>(body, null, HttpStatus.OK);
