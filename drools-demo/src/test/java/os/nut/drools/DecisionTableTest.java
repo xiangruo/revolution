1998aa017d229fb6689df611331d24d336e3d016
=xiangruo@outlook.com <=xiangruo@outlook.com>
2016-01-28 15:49:59 +0800

    drools demo

---
+++ drools-demo/src/test/java/os/nut/drools/DecisionTableTest.java
@@ -0,0 +1,21 @@
+package os.nut.drools;
+
+import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DecisionTableTest extends DroolsBaseTest {
+
+	@Test
+	public void test() {
+		KieSession kSession = kieContainer.newKieSession("ksession-dtables");
+		Message message = new Message();
+		message.setMessage("Hello World");
+		message.setStatus(Message.HELLO);
+		kSession.insert(message);
+		kSession.fireAllRules();
+	}
+
+}
---
+++ drools-demo/src/test/java/os/nut/drools/DroolsBaseTest.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright © 2014 Xiong Zhijun, All Rights Reserved.
+ * Email : hust.xzj@gmail.com
+ */
+package os.nut.drools;
+
+import org.junit.Before;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+
+/**
+ * base
+ */
+public abstract class DroolsBaseTest {
+
+	protected KieServices kieServices;
+
+	protected KieContainer kieContainer;
+
+	@Before
+	public void setUp() {
+		kieServices = KieServices.Factory.get();
+		kieContainer = kieServices.getKieClasspathContainer();
+	}
+
+}
